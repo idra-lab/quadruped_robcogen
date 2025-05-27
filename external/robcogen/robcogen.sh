33f1f3a564b790bb9495e6a1da8ce06c1ff20bab
Marco Camurri <marco.camurri@unitn.it>
2025-05-27 15:45:33 +0200

    fix java

--- external/robcogen/robcogen.sh
+++ external/robcogen/robcogen.sh
@@ -4,5 +4,5 @@ ROBCOGEN_DIR=$(rospack find quadruped_robcogen)/external/robcogen
 CLASSPATH=`find ${ROBCOGEN_DIR}/lib/ -name '*.jar' | tr '\n' ':'`
 
 echo "java -Dlog4j.configuration=file:log4j.cfg -classpath ${CLASSPATH}:${ROBCOGEN_DIR}/bin/ iit.robcogen.Generator $1 $2 $3"
-java -Dlog4j.configuration=file:${ROBCOGEN_DIR}/log4j.cfg -classpath ${CLASSPATH}:${ROBCOGEN_DIR}/bin/ iit/robcogen/Generator $1 $2 $3
+java --add-opens java.base/java.lang=ALL-UNNAMED -Dlog4j.configuration=file:${ROBCOGEN_DIR}/log4j.cfg -classpath ${CLASSPATH}:${ROBCOGEN_DIR}/bin/ iit/robcogen/Generator $1 $2 $3
 
